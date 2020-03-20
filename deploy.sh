64add0976ed11ff73eef01346049e422bb6fc1ec
Valentin Novikov <isnotvalya@no-email.host>
2020-03-20 14:18:16 +0500

    add kafka, mariadb

--- deploy.sh
+++ deploy.sh
@@ -27,10 +27,26 @@ MONGO_SERVICE_NAME=$MONGO_NAME
 MONGO_FORWARD_PORT="27017:27017"
 MONGO_HELM_VALUES_FILE=mongo/values.yaml
 
+# MariaDB
+MARIADB_NAME=mariadb
+MARIADB_SERVICE_NAME=$MARIADB_NAME
+MARIADB_FORWARD_PORT="3306:3306"
+MARIADB_HELM_VALUES_FILE=mariadb/values.yaml
+
 # Redis
 REDIS_NAME=redis
 REDIS_SERVICE_NAME=$REDIS_NAME
 REDIS_HELM_VALUES_FILE=redis/values.yaml
+
+# Kafka
+KAFKA_NAME=kafka
+KAFKA_SERVICE_NAME=$KAFKA_NAME
+KAFKA_HELM_VALUES_FILE=kafka/values.yaml
+
+KAFDROP_NAME=kafdrop
+KAFDROP_SERVICE_NAME=${KAFDROP_NAME}
+KAFDROP_FORWARD_PORT="9000:9000"
+KAFDROP_HELM_VALUES_FILE=_helm2/kafdrop/values.yaml
 
 #
 _kubectl() {
@@ -51,10 +67,10 @@ _helm_install_db() {
   _helm install --namespace $NAMESPACE_DB $*
 }
 
-# k8s_create_namespaces() {
-#   _kubectl create namespace $NAMESPACE
-#   _kubectl create namespace $NAMESPACE_DB
-# }
+k8s_create_namespaces() {
+  _kubectl create namespace $NAMESPACE
+  _kubectl create namespace $NAMESPACE_DB
+}
 k8s_drop_namespaces() {
   _kubectl delete namespace $NAMESPACE
   _kubectl delete namespace $NAMESPACE_DB
@@ -74,8 +90,7 @@ pg_deploy() {
   echo "TODO: --set ingress.hosts[0].host=$ingress_host"
   echo "TODO: --set serverDefinitions.servers"
 
-  _helm_install_db --name $PGADMIN_SERVICE_NAME -f $PGADMIN_HELM_VALUES_FILE \
-    stable/pgadmin
+  _helm_install_db --name $PGADMIN_SERVICE_NAME -f $PGADMIN_HELM_VALUES_FILE stable/pgadmin
 }
 
 pg_deploy_rs() {
@@ -91,8 +106,7 @@ pg_deploy_rs() {
   echo "TODO: --set ingress.hosts[0].host=$ingress_host"
   echo "TODO: --set serverDefinitions.servers"
 
-  _helm_install_db --name $PGADMIN_SERVICE_NAME -f $PGADMIN_HELM_VALUES_FILE \
-    stable/pgadmin
+  _helm_install_db --name $PGADMIN_SERVICE_NAME -f $PGADMIN_HELM_VALUES_FILE stable/pgadmin
 }
 
 pg_drop_deploy() {
@@ -153,10 +167,62 @@ redis_drop_deploy() {
   _helm delete --purge $REDIS_NAME
 }
 
+#
+kafka_deploy() {
+  local ns=${NAMESPACE_DB}
+
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+  _helm_install_db --name $KAFKA_NAME -f $KAFKA_HELM_VALUES_FILE bitnami/kafka
+}
+
+kafka_drop() {
+  _helm delete --purge $KAFKA_NAME
+}
+
+kafdrop_deploy() {
+  # local ns=${NAMESPACE_DB}
+
+  _helm_install_db --name $KAFDROP_NAME -f $KAFDROP_HELM_VALUES_FILE \
+    --set kafka.brokerConnect=${KAFKA_NAME}:9092 \
+    ./_helm2/kafdrop
+}
+
+kafdrop_drop() {
+  _helm delete --purge $KAFDROP_NAME
+}
+
+kafdrop_forward() {
+  local ns=${NAMESPACE_DB}
+
+  kubectl --namespace $ns port-forward svc/${KAFDROP_SERVICE_NAME} ${KAFDROP_FORWARD_PORT}
+}
+
+#
+mariadb_deploy() {
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+  _helm_install_db --name $MARIADB_NAME -f $MARIADB_HELM_VALUES_FILE bitnami/mariadb
+}
+
+mariadb_drop() {
+  _helm delete --purge $MARIADB_NAME
+}
+
+mariadb_forward() {
+  local ns=${NAMESPACE_DB}
+
+  kubectl --namespace $ns port-forward svc/${MARIADB_SERVICE_NAME} ${MARIADB_FORWARD_PORT}
+}
+#
+
+drop_all() {
+  echo TODO
+}
+
 
 case "$1" in
   ns-create) k8s_create_namespaces ;;
   ns-delete) k8s_drop_namespaces ;;
+  # drop-all) drop_all ;;
   pg-up) pg_deploy ;;
   pg-rs-up) pg_deploy_rs ;;
   pg-drop) pg_drop_deploy ;;
@@ -168,6 +234,14 @@ case "$1" in
   mongo-forward) mongo_forward ;;
   redis-deploy) redis_deploy ;;
   redis-drop) redis_drop_deploy ;;
+  kafka-deploy) kafka_deploy ;;
+  kafka-drop) kafka_drop ;;
+  kafdrop-deploy) kafdrop_deploy ;;
+  kafdrop-drop) kafdrop_drop ;;
+  kafdrop-forward) kafdrop_forward ;;
+  mariadb-deploy) mariadb_deploy ;;
+  mariadb-drop) mariadb_drop ;;
+  mariadb-forward) mariadb_forward ;;
   *)
     NS=$NAMESPACE
     NSDB=$NAMESPACE_DB
@@ -175,6 +249,7 @@ case "$1" in
     echo "\nCommands:"
     echo "  ns-create   - create namespace '$NS', '$NSDB'"
     echo "  ns-delete   - delete namespace '$NS', '$NSDB"
+    # echo "  drop-all    - drop all services in namespace '$NS' and '$NSDB' (namespaces not remove)"
     echo
     echo "PostgreSQL"
     echo "  pg-up       - deploy PostgreSQL and PgAdmin in namespace '$NSDB'"
@@ -184,6 +259,11 @@ case "$1" in
     echo "  pg-forward  - postgres forward port"
     echo "  pg-print-password  - print postgres password"
     echo
+    echo "MariaDB"
+    echo "  mariadb-deploy   - deploy MariaDB (namespace '$NSDB')"
+    echo "  mariadb-drop     - drop MariaDB (namespace '$NSDB')"
+    echo "  mariadb-forward  - forwarding port $MARIADB_FORWARD_PORT (namespace '$NSDB')"
+    echo
     echo "MongoDB"
     echo "  mongo-deploy   - deploy MongoDB (namespace '$NSDB')"
     echo "  mongo-drop     - drop MongoDB (namespace '$NSDB')"
@@ -192,6 +272,13 @@ case "$1" in
     echo "Redis"
     echo "  redis-deploy   - deploy Redis (namespace '$NSDB')"
     echo "  redis-drop     - drop Redis (namespace '$NSDB')"
+    echo
+    echo "Kafka"
+    echo "  kafka-deploy     - deploy Kafka (namespace '$NSDB')"
+    echo "  kafka-drop       - drop Kafka (namespace '$NSDB')"
+    echo "  kafdrop-deploy   - deploy Kafka UI (namespace '$NSDB')"
+    echo "  kafdrop-drop     - drop Kafka UI (namespace '$NSDB')"
+    echo "  kafdrop-forward  - forwarding port $KAFDROP_SERVICE_NAME (namespace '$NSDB')"
     echo
     exit 1
   ;;
